bee4bc6ff7020ea0ee9f4d8aafaf207c646769ae
Stefan Heller <stefan.heller92@gmail.com>
2021-02-13 10:37:24 +0100

    initial

---
+++ Site.js
@@ -0,0 +1,32 @@
+function play(src) {
+    var audio = document.getElementsByClassName(src);
+
+    for (let item of audio) {
+        item.play();
+    }
+}
+
+function stop(src) {
+    var audio = document.getElementsByClassName(src);
+
+    for (let item of audio) {
+        item.pause();
+        item.currentTime = 0;
+    }
+}
+
+
+
+function mute(src, btn) {
+    var audio = document.getElementById(src);
+    var button = document.getElementById(btn);
+    if (audio.muted === false) {
+        audio.muted = true;
+        button.style.height = '150px';
+        button.style.width = '150px';
+    } else {
+        audio.muted = false;
+        button.style.height = '300px';
+        button.style.width = '300px';
+    }
+}
